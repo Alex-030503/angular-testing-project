63fe275654cf6d37427db1ff4c51612b5f304593
Alex-030503 <alejandragarcia040@gmail.com>
2025-04-08 20:22:12 -0600

    prueba terminada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,38 @@
 import { Component } from '@angular/core';
+import { TaskListComponent } from './task-list/task-list.component';
+import { Task } from './task';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
+  imports: [CommonModule, FormsModule, TaskListComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+updateTask() {
+throw new Error('Method not implemented.');
+}
   title = 'angular-testing-project';
 
-  addTask(task: string) {
+  tasks: Task[]=[];
+  newTask:string='';
 
+
+ /* addTask(task: string) { pq asi no me sirvee :(*/ 
+ addTask() {
+    if(this.newTask){
+      this.tasks.push({nombre:this.newTask})
+      this.newTask='';
+      localStorage.setItem('Lista de tareas', JSON.stringify(this.tasks));
+    }
+
+  }
+  
+  storageKey(storageKey: any, arg1: string) {
+    throw new Error('Method not implemented.');
+    localStorage.setItem(storageKey,JSON.stringify(this.tasks));
   }
 
   deleteTask(task: string) {
--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Task } from '../task';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-task-list',
+  imports:[CommonModule,FormsModule],
   templateUrl: './task-list.component.html',
   styleUrls: ['./task-list.component.css']
 })
 export class TaskListComponent {
+  @Input() task!:Task;
+  @Input() indice!: number;
 
-  tasks: string[] = [];
+  tasks: Task[]=[];
+  newTask:string='';
+
+  @Output() actualizar = new EventEmitter<{ task: Task;}>();
+
+  editar: boolean = false;
+  nomEditado: string = '';
+
+ /* tasks: string[] = [];*/
 
   constructor() {
     console.log('TaskListComponent initialized');
@@ -17,9 +32,17 @@ export class TaskListComponent {
     console.log('Adding task:', task);
   }
 
-  updateTask(task: string) {
-    console.log('Updating task:', task);
+  /*updateTask(task: string) {*/
+  updateTask() {
+    console.log('Updating task:', this.task);
+    this.tasks.push({nombre:this.newTask})
+    this.editar = true;
+    this.nomEditado = this.task.nombre;
+
   }
+
+
+
 
   deleteTask(task: string) {
     console.log('Deleting task:', task);
